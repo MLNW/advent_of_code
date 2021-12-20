87cd9840fa909c299ba0c12f1c31f751ac7f73b9
Lucas Resch <lucas.resch@gmx.de>
2021-12-20 22:28:34 +0100

    003: Add solution for part 1

---
+++ calendar/003-go/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"advent-of-code-2021/utils/files"
+	"fmt"
+	"strconv"
+)
+
+func parseInput(lines []string) [][2]int {
+	var counters [][2]int
+	for i, line := range lines {
+		for j, char := range line {
+			if i == 0 {
+				counters = append(counters, [2]int{0, 0})
+			}
+			if char == '0' {
+				counters[j][0]++
+			} else {
+				counters[j][1]++
+			}
+		}
+	}
+	return counters
+}
+
+func calcPowerConsumption(counters [][2]int) int64 {
+	var gammaRate string
+	var epsilonRate string
+	for _, counter := range counters {
+		if counter[0] > counter[1] {
+			gammaRate = gammaRate + "0"
+			epsilonRate = epsilonRate + "1"
+		} else {
+			gammaRate = gammaRate + "1"
+			epsilonRate = epsilonRate + "0"
+		}
+	}
+	gamma := convertBinToDec(gammaRate)
+	epsilon := convertBinToDec(epsilonRate)
+	fmt.Printf("gamma: %s\nepsilon: %s\n", gammaRate, epsilonRate)
+	return gamma * epsilon
+}
+
+func convertBinToDec(bin string) int64 {
+	dec, err := strconv.ParseInt(bin, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+	return dec
+}
+
+func main() {
+	lines := files.ReadLines("input.txt")
+	counters := parseInput(lines)
+	for _, counter := range counters {
+		fmt.Printf("[%d, %d]", counter[0], counter[1])
+	}
+	fmt.Println()
+	power := calcPowerConsumption(counters)
+	fmt.Println(power)
+}
---
+++ calendar/003-go/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"testing"
+)
+
+var lines = []string{
+	"00100",
+	"11110",
+	"10110",
+	"10111",
+	"10101",
+	"01111",
+	"00111",
+	"11100",
+	"10000",
+	"11001",
+	"00010",
+	"01010",
+}
+
+var counters = [][2]int{
+	{5, 7}, {7, 5}, {4, 8}, {5, 7}, {7, 5},
+}
+
+func TestParseInput(t *testing.T) {
+	result := parseInput(lines)
+
+	for i := 0; i < len(counters); i++ {
+		if result[i] != counters[i] {
+			t.Logf("Expected [%d,%d], but got [%d,%d]",
+				counters[i][0], counters[i][1],
+				result[i][0], result[i][1])
+			t.Fail()
+		}
+	}
+}
+
+func TestSolution(t *testing.T) {
+	result := calcPowerConsumption(counters)
+
+	expected := 198
+	if result != int64(expected) {
+		t.Fatalf("Expected %d, but got %d", expected, result)
+	}
+}
