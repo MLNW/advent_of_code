a7e8365fce37601391bbf45a2a6a0b36180a6ed1
Lucas Resch <lucas.resch@gmx.de>
2022-12-05 12:41:11 +0100

    2022-05: Solve first puzzle

--- 2022/lib/puzzles/day05/solution.ex
+++ 2022/lib/puzzles/day05/solution.ex
@@ -4,13 +4,81 @@ defmodule Puzzles.Day05 do
   end
 
   defp parse_input(input) do
-    input
-    |> String.split("\n")
-    |> Enum.filter(&(&1 != ""))
+    [stacks | [numbering | [instructions]]] =
+      input
+      |> String.split("\n")
+      |> Enum.chunk_by(fn line -> String.trim_leading(line) |> String.starts_with?("1") end)
+
+    {parse_stacks(stacks, numbering), parse_instructions(instructions)}
+  end
+
+  defp parse_stacks(stacks, numbering) do
+    numbering =
+      numbering
+      |> Enum.map(&String.split/1)
+      |> List.first()
+      |> Enum.map(&String.to_integer/1)
+
+    lines =
+      stacks
+      |> Enum.map(&parse_stack_line([], &1, length(numbering)))
+
+    for index <- numbering, line <- lines do
+      {element, _rest} = line |> List.pop_at(index - 1)
+      {index, element}
+    end
+    |> Enum.filter(fn {_index, x} -> x != nil end)
+    |> Enum.reduce(%{}, fn {index, x}, acc ->
+      Map.update(acc, index, [x], fn current -> current ++ [x] end)
+    end)
+  end
+
+  defp parse_stack_line(stack, "", n) do
+    case length(stack) == n do
+      true -> stack
+      # Ensure all lines have the same amount of elements
+      false -> parse_stack_line(stack ++ [nil], "", n)
+    end
+  end
+
+  defp parse_stack_line(stack, line, n) do
+    {element, rest} = String.split_at(line, 4)
+
+    element = element |> String.trim() |> String.at(1)
+
+    parse_stack_line(stack ++ [element], rest, n)
+  end
+
+  defp parse_instructions(instructions) do
+    instructions
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.map(fn line -> String.split(line, ~r/\D+\s/, trim: true) end)
+    |> Enum.map(fn line -> line |> Enum.map(&String.to_integer/1) end)
   end
 
   def part_one(input \\ nil) do
-    input
-    |> parse_input()
+    result =
+      input
+      |> parse_input()
+      |> execute()
+
+    for {_, v} <- result, into: [] do
+      List.first(v, "")
+    end
+    |> Enum.reduce("", fn x, acc -> acc <> x end)
+  end
+
+  defp execute({stacks, []}), do: stacks
+
+  defp execute({stacks, [instruction | instructions]}) do
+    execute({execute_instruction(instruction, stacks), instructions})
+  end
+
+  defp execute_instruction([0 | _], stacks), do: stacks
+
+  defp execute_instruction([move | [from | [to]]], stacks) do
+    {value, stacks} = Map.get_and_update(stacks, from, fn [v | rest] -> {v, rest} end)
+    stacks = Map.update!(stacks, to, fn current -> [value] ++ current end)
+    execute_instruction([move - 1, from, to], stacks)
   end
 end
--- 2022/test/day05_test.exs
+++ 2022/test/day05_test.exs
@@ -3,12 +3,20 @@ defmodule Day05Test do
   import TestHelpers
 
   @input """
+      [D]
+  [N] [C]
+  [Z] [M] [P]
+   1   2   3
 
+  move 1 from 2 to 1
+  move 3 from 1 to 3
+  move 2 from 2 to 1
+  move 1 from 1 to 2
 
   """
 
-  aoc_test(0, Puzzles.Day05.part_one(@input))
-  # aoc_test(0, Puzzles.Day05.part_one())
+  aoc_test("CMZ", Puzzles.Day05.part_one(@input))
+  aoc_test("VJSFHWGFT", Puzzles.Day05.part_one())
 
   # aoc_test(0, Puzzles.Day05.part_two(@input))
   # aoc_test(0, Puzzles.Day05.part_two())
