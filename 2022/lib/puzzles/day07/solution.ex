d6e4868e4457bbc927835871143a663dec63460d
Lucas Resch <lucas.resch@gmx.de>
2022-12-06 23:09:18 +0100

    2022-07: Prepare puzzle

---
+++ 2022/lib/puzzles/day07/solution.ex
@@ -0,0 +1,10 @@
+defmodule Puzzles.Day07 do
+  defp parse_input(input) when is_nil(input), do: Common.Utils.read_input(7) |> parse_input()
+
+  defp parse_input(input) do
+    input
+  end
+
+  def part_one(input \\ nil) do
+  end
+end
---
+++ 2022/test/day07_test.exs
@@ -0,0 +1,11 @@
+defmodule Day07Test do
+  use ExUnit.Case
+  import TestHelpers
+
+  @input """
+
+
+  """
+
+  aoc_test(0, Puzzles.Day07.part_one())
+end
