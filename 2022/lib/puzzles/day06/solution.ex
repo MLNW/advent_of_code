b2bfa58407afdae6a41c357f72a8887118e51275
Lucas Resch <lucas.resch@gmx.de>
2022-12-06 07:29:03 +0100

    2022-06: Solve first puzzle

--- 2022/lib/puzzles/day06/solution.ex
+++ 2022/lib/puzzles/day06/solution.ex
@@ -5,11 +5,17 @@ defmodule Puzzles.Day06 do
 
   defp parse_input(input) do
     input
+    |> String.to_charlist()
   end
 
   def part_one(input \\ nil) do
     input
     |> parse_input()
+    |> Enum.chunk_every(4, 1)
+    |> Enum.map(&Enum.uniq/1)
+    |> Enum.with_index(4)
+    |> Enum.find(fn {chars, _index} -> length(chars) == 4 end)
+    |> elem(1)
   end
 
   def part_two(input \\ nil) do
--- 2022/test/day06_test.exs
+++ 2022/test/day06_test.exs
@@ -3,12 +3,12 @@ defmodule Day06Test do
   import TestHelpers
 
   @input """
-
+  mjqjpqmgbljsphdztnvjfqwrcgsmlb
 
   """
 
-  aoc_test("CMZ", Puzzles.Day06.part_one(@input))
-  # aoc_test("VJSFHWGFT", Puzzles.Day06.part_one())
+  aoc_test(7, Puzzles.Day06.part_one(@input))
+  aoc_test(1779, Puzzles.Day06.part_one())
 
   # aoc_test("MCD", Puzzles.Day06.part_two(@input))
   # aoc_test("LCTQFBVZV", Puzzles.Day06.part_two())
