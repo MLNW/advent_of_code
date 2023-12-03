57acb34a9621210a64ace2be07a23d60b0e71c7e
Lucas Resch <lucas.resch@gmx.de>
2023-12-03 15:08:43 +0100

    2023-03: Solve second puzzle

--- 2023-rust/README.md
+++ 2023-rust/README.md
@@ -9,11 +9,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `63.8µs` | `626.0µs` |
-| [Day 2](./src/bin/02.rs) | `40.1µs` | `46.9µs` |
-| [Day 3](./src/bin/03.rs) | `424.5µs` | `-` |
+| [Day 1](./src/bin/01.rs) | `64.7µs` | `634.0µs` |
+| [Day 2](./src/bin/02.rs) | `40.8µs` | `46.7µs` |
+| [Day 3](./src/bin/03.rs) | `426.2µs` | `429.9µs` |
 
-**Total: 1.20ms**
+**Total: 1.64ms**
 <!--- benchmarking table --->
 
 ## Usage
--- 2023-rust/src/bin/03.rs
+++ 2023-rust/src/bin/03.rs
@@ -4,13 +4,18 @@ use regex::Regex;
 
 advent_of_code::solution!(3);
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 struct Coordinate {
     x: usize,
     y: usize,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Eq, PartialEq, Hash)]
+struct Symbol {
+    character: char,
+    position: Coordinate,
+}
+
 struct EnginePart {
     number: u32,
     start: Coordinate,
@@ -50,34 +55,59 @@ impl EnginePart {
         neighbors
     }
 
-    pub fn is_valid(&self, symbols: &HashMap<Coordinate, Symbol>) -> bool {
+    pub fn neighboring_symbol(&self, symbols: &HashMap<Coordinate, Symbol>) -> Option<Symbol> {
         for neighbor in self.neighbors() {
-            if symbols.contains_key(&neighbor) {
-                return true;
+            if let Some(symbol) = symbols.get(&neighbor) {
+                return Some(symbol.clone());
             }
         }
-        false
+        None
     }
-}
-
-#[derive(Debug)]
-struct Symbol {
-    character: char,
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let parts = parse_engine_parts(input);
-    let symbols: HashMap<Coordinate, Symbol> = parse_symbols(input);
+    let symbols: HashMap<Coordinate, Symbol> = parse_symbols(input, r"(\+|-|\*|/|=|\$|@|%|#|&)");
 
     parts
         .iter()
-        .filter(|part| part.is_valid(&symbols))
+        .filter(|part| part.neighboring_symbol(&symbols).is_some())
         .map(|part| part.number)
         .reduce(|acc, number| acc + number)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let parts = parse_engine_parts(input);
+    let symbols: HashMap<Coordinate, Symbol> = parse_symbols(input, r"\*");
+
+    parts
+        .iter()
+        .map(|part| (part.neighboring_symbol(&symbols), part))
+        .filter_map(|(symbol, part)| symbol.map(|s| (s, part)))
+        // Group by neighboring symbol
+        .fold(
+            HashMap::new(),
+            |mut acc: HashMap<Symbol, Vec<&EnginePart>>, (symbol, part)| {
+                acc.entry(symbol).or_default().push(part);
+                acc
+            },
+        )
+        .iter()
+        .filter_map(|(_, parts)| {
+            if parts.len() == 2 {
+                return Some(parts);
+            }
+            None
+        })
+        // Calculate gear ratio
+        .map(|parts| {
+            parts
+                .iter()
+                .map(|part| part.number)
+                .reduce(|acc, number| acc * number)
+                .unwrap()
+        })
+        .reduce(|acc, gear_ratio| acc + gear_ratio)
 }
 
 fn parse_engine_parts(input: &str) -> Vec<EnginePart> {
@@ -101,18 +131,20 @@ fn parse_engine_parts(input: &str) -> Vec<EnginePart> {
     parts
 }
 
-fn parse_symbols(input: &str) -> HashMap<Coordinate, Symbol> {
+fn parse_symbols(input: &str, pattern: &str) -> HashMap<Coordinate, Symbol> {
     let mut symbols: HashMap<Coordinate, Symbol> = HashMap::new();
-    let regex = Regex::new(r"(\+|-|\*|/|=|\$|@|%|#|&)").unwrap();
+    let regex = Regex::new(pattern).unwrap();
     for (y, line) in input.lines().enumerate() {
         for symbol_match in regex.find_iter(line) {
+            let position = Coordinate {
+                x: symbol_match.start(),
+                y,
+            };
             symbols.insert(
-                Coordinate {
-                    x: symbol_match.start(),
-                    y,
-                },
+                position,
                 Symbol {
                     character: symbol_match.as_str().chars().next().unwrap(),
+                    position,
                 },
             );
         }
@@ -133,6 +165,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
